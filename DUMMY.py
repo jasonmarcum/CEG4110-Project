acdd8b8d460ffa9329729476ae1ffc3a33f3cd88
Aaron <aaron.eberhart@gmail.com>
2017-11-17 17:54:01 -0500

    DUMMY.py file
    So you don't need the AI if you don't want.
    It's the same exact output as the findFood.py, but just makes up random numbers

---
+++ DUMMY.py
@@ -0,0 +1,22 @@
+import sys
+import random
+import re
+
+print("SETTING UP AI")
+
+def batch():
+	listy = []
+	for argument in sys.argv:
+		if re.match('.*DUMMY.py.*',argument):
+			continue
+		else:
+			listy.append([random.uniform(-2,8),random.uniform(-2,8)])
+	return listy
+	
+print("OPENING FILE")
+filew = open("output.txt", 'w')
+print("ANALYZING IMAGES")
+filew.write(str(batch()))
+filew.close()
+
+print ("DONE FINDING FOOD")
