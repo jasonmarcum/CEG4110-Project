e9ee0924b9701528ca2cb56eabdda3b01e3fab8a
blulegume <31670982+blulegume@users.noreply.github.com>
2017-11-18 15:00:25 -0500

    Store files in DB

--- upload.php
+++ upload.php
@@ -1,23 +1,23 @@
 
 <?php
 
+/* TODO
+Better validation of images, i.e. file size, file type, etc
+Integrate this code into uploads page for GUI
+*/
+
 echo ' 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html> <h1>SEEFOOD</h1>';
 //<img src="/images/image1.png" width="280" height="125" title="Logo of a company" alt="Logo of a company" />
 
-
-
-
 $target_dir = "/var/www/html/uploads/";
-
-//echo print_r($_FILES);
+$all_images = "";
 $target = $_POST["submit"];
 foreach($_FILES['uploads']['tmp_name'] as $key => $tmp_name){
 	$filename = $_FILES['uploads']['name'][$key];
 	$target_file = $target_dir . basename($filename);
     $uploadOk = 1;
-    //echo "</br>" . $filename . " is the file name </br>";
 	//echo $target_file . " is the full path </br>";
 	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 	// Check if image file is a actual image or fake image
@@ -32,73 +32,79 @@ foreach($_FILES['uploads']['tmp_name'] as $key => $tmp_name){
 		if (move_uploaded_file($_FILES['uploads']['tmp_name'][$key], $target_file)) {
 			//echo "The file ". $filename . " has been uploaded.</br>";
 		} else {
-			//echo "There was an error uploading your file.</br>";
+			echo "There was an error uploading your file.</br>";
         }
         
-        
-        
-        
         echo "<img src='/uploads/$filename' height='400'>";
-        //echo '<img src="/uploads/magritte2.jpg">';
-        $python = `python findFood.py {$target_file}`;
-        echo '</br>' . $python . '</br>';
+
+        $all_images .= ('uploads/' . $filename . " ");
+}
+//echo "</br>All images: " . $all_images . "</br>";
+$python = `python findFood4.py {$all_images}`;
+
+$mysqli = new mysqli("localhost", "root", "seefood", "ProcessedImages");
+if ($mysqli->connect_error) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_error . ") " . $mysqli->connect_error;
+}
+//echo "connected successfully";
+// each image run and scores are in output.txt
+$myfile = fopen("output4.txt", "rw") or die("unable to open file");
+
+$index = 0;
+foreach($_FILES['uploads']['tmp_name'] as $key => $tmp_name){
+	// get url
+	$filename = $_FILES['uploads']['name'][$key];
+	$url = "uploads/" . $filename;
+
+	$food_score = fgets($myfile);
+	$not_food_score = fgets($myfile);
+	//echo $score1 . "</br>" . $score2 . "</br>";
+	// turn food scores into floats
+	$food_score = floatval($food_score);
+	$not_food_score = floatval($not_food_score);
+	$composite_score = abs($food_score - $not_food_score);
+
+	// is it food?
+	if(abs($food_score) > abs($not_food_score)){
+		$is_food = 1;
+	} else {$is_food = 0;}
+
+	$ip = getenv('HTTP_CLIENT_IP')?:
+	getenv('HTTP_X_FORWARDED_FOR')?:
+	getenv('HTTP_X_FORWARDED')?:
+	getenv('HTTP_FORWARDED_FOR')?:
+	getenv('HTTP_FORWARDED')?:
+	getenv('REMOTE_ADDR');
+	
+	//echo "IP is : " . $ip . '</br>';
+
+
+	$sql = "INSERT INTO Image_Info (url, food_score, not_food_score,
+	is_food, ip, composite_score)
+	VALUES ('$url', '$food_score', '$not_food_score', '$is_food', 
+			'$ip', '$composite_score')";
+	
+	//echo "</br>SQL Query: $sql </br>";
+
+	if($mysqli->query($sql) === FALSE){
+		echo "Error: " . $sql . "<br>" . $mysqli->error;
+	}
+
 }
 
+// $sql = "SELECT * FROM Image_Info WHERE is_food = 0 ORDER BY composite_score";
+// if(!$result = $mysqli->query($sql)){
+//     die('There was an error running the query [' . $mysqli->error . ']');
+// }
+
+// while($row = $result->fetch_assoc()){
+//     echo "</br>" . $row['url'] . $row['food_score']. $row['not_food_score'] . '</br>';
+// }
+
+$mysqli->close();
+fclose($myfile);
 echo '</html>';
 
-// 	// Check if file already exists
-// 	if (file_exists($target_file)) {
-// 		//echo "Sorry, file already exists.";
-// 		$uploadOk = 0;
-// 	}
-
-// 	///////////////
-// 	//echo "file is unique";
-// 	///////////////
-
-// 	// Check file size
-// 	if ($_FILES["fileToUpload"]["size"] > 5000000) {
-// 		echo "Your file is too large.</br>";
-// 		$uploadOk = 0;
-// 	}
-
-// 	///////////////
-// 	//echo "size is ok";
-// 	///////////////
-
-// 	// Allow certain file formats
-// 	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-// 	&& $imageFileType != "gif" ) {
-// 		echo "Only JPG, JPEG, PNG & GIF files are allowed.</br>";
-// 		$uploadOk = 0;
-// 	}
-// 	// Check if $uploadOk is set to 0 by an error
-// 	if ($uploadOk == 0) {
-// 		echo "Your file was not uploaded.\n";
-// 	// if everything is ok, try to upload file
-// 	} else {
-// 		//echo "well we got here";
-
-// //}
-// //echo $target_file;
-// ////////////////////////////
-// //echo $target_file;
-// //$command = escapeshellcmd('~/seefood-core-ai/find_food.py ' . $target_file);
-// //$output = shell_exec($command);
-// //echo "\n" . $command;
-// //echo $output;
-
-// //exec('sudo -u www-data python ~/seefood-core-ai/find_food.py ' . $target_file); 
-
-// $python = `python findFood.py {$target_file}`;
-// echo $python;
-
-// //$mysqli = new mysqli("54.88.201.92", "root", "seefood", "ProcessedImages");
-// //$result = $mysqli->query("SELECT 'Hello, dear MySQL user!' AS _message FROM DUAL");
-// //$row = $result->fetch_assoc();
-// //echo htmlentities($row['_message']);
- 
-// //echo "now you're connected to the db";
 ?>
 
 
